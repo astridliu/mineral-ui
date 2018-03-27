0c62a2666d5b3e59e2d828843b1a513651bbf8d4
Victoria Vasys <victoria.vasys@gmail.com>
2018-03-27 16:33:32 -0600

    chore(website): Add Text demo & examples
    Co-authored-by: Kyle Gach <kyle.gach@ca.com>

---
+++ src/website/app/demos/Text/examples/index.js
@@ -0,0 +1,22 @@
+/* @flow */
+import alignment from './alignment';
+import appearance from './appearance';
+import color from './color';
+import fontWeight from './fontWeight';
+import headings from './headings';
+import importSyntax from './importSyntax';
+import margin from './margin';
+import rtl from './rtl';
+import text from './text';
+
+export default [
+  importSyntax,
+  text,
+  headings,
+  appearance,
+  margin,
+  alignment,
+  fontWeight,
+  color,
+  rtl
+];
